53fe1c8aa4669d15af8cb4bff211c25836a24d1a
KevinTang <tjtzc@126.com>
2017-05-23 16:10:14 +0800

    init

---
+++ ex.py
@@ -0,0 +1,141 @@
+﻿# !/usr/bin/env python 
+ 
+ #  Demonstrate how to use the vtkBoxWidget to control volume rendering 
+#  within the interior of the widget. 
+ 
+import  vtk
+from  vtk.util.misc  import  vtkGetDataRoot
+VTK_DATA_ROOT  =  vtkGetDataRoot()
+
+dicomReader  =  vtk.vtkDICOMImageReader()
+dicomReader.SetDirectoryName('../CT/')
+dicomReader.Update()
+dataSpacing  =  dicomReader.GetDataSpacing()
+# 由于volumeMapper之能接收无符号数，所以必须加以转换 
+srange  =  dicomReader.GetOutput().GetScalarRange()
+min  =  srange[0]
+max  =  srange[ 1 ]
+
+diff  =  max - min
+slope  =   40000 / diff
+inter  =   - slope * min
+shift  =  inter / slope
+
+shifter  =  vtk.vtkImageShiftScale()
+shifter.SetShift(shift)
+shifter.SetScale(slope)
+shifter.SetOutputScalarTypeToUnsignedShort()
+shifter.SetInputConnection(dicomReader.GetOutputPort())
+shifter.ReleaseDataFlagOff()
+shifter.Update()
+print  min,max,slope,inter,shift
+
+ #  Load a volume, use the widget to control what's volume 
+#  rendered. Basically the idea is that the vtkBoxWidget provides a box 
+#  which clips the volume rendering. 
+ 
+tfun  =  vtk.vtkPiecewiseFunction()
+tfun.AddPoint( 70.0 ,  0.0 )
+tfun.AddPoint( 599.0 , 0)
+tfun.AddPoint( 600.0 , 0)
+tfun.AddPoint( 1195.0 , 0)
+tfun.AddPoint( 1200 , .2 )
+tfun.AddPoint( 1300 , .3 )
+tfun.AddPoint( 2000 , .3 )
+tfun.AddPoint( 4095.0 ,  1.0 )
+
+ctfun  =  vtk.vtkColorTransferFunction()
+ctfun.AddRGBPoint( 0.0 ,  0.5 ,  0.0 ,  0.0 )
+ctfun.AddRGBPoint( 600.0 ,  1.0 ,  0.5 ,  0.5 )
+ctfun.AddRGBPoint( 1280.0 ,  0.9 ,  0.2 ,  0.3 )
+ctfun.AddRGBPoint( 1960.0 ,  0.81 ,  0.27 ,  0.1 )
+ctfun.AddRGBPoint( 4095.0 ,  0.5 ,  0.5 ,  0.5 )
+ #
+compositeFunction  =  vtk.vtkVolumeRayCastCompositeFunction()
+volumeMapper  =  vtk.vtkVolumeRayCastMapper()
+volumeMapper.SetInputConnection(shifter.GetOutputPort())
+volumeMapper.SetVolumeRayCastFunction(compositeFunction)
+
+volumeProperty  =  vtk.vtkVolumeProperty()
+volumeProperty.SetColor(ctfun)
+volumeProperty.SetScalarOpacity(tfun)
+volumeProperty.SetInterpolationTypeToLinear()
+volumeProperty.ShadeOn()
+
+newvol  =  vtk.vtkVolume()
+newvol.SetMapper(volumeMapper)
+newvol.SetProperty(volumeProperty)
+
+outline  =  vtk.vtkOutlineFilter()
+outline.SetInputConnection(shifter.GetOutputPort())
+outlineMapper  =  vtk.vtkPolyDataMapper()
+outlineMapper.SetInputConnection(outline.GetOutputPort())
+outlineActor  =  vtk.vtkActor()
+outlineActor.SetMapper(outlineMapper)
+
+ #  Create the RenderWindow, Renderer and both Actors 
+ren  =  vtk.vtkRenderer()
+renWin  =  vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+iren  =  vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+ #  The SetInteractor method is how 3D widgets are associated with the 
+#  render window interactor. Internally, SetInteractor sets up a bunch 
+#  of callbacks using the Command/Observer mechanism (AddObserver()). 
+boxWidget  =  vtk.vtkBoxWidget()
+boxWidget.SetInteractor(iren)
+boxWidget.SetPlaceFactor( 1.0 )
+
+ #  Add the actors to the renderer, set the background and size 
+ren.AddActor(outlineActor)
+ren.AddVolume(newvol)
+
+ren.SetBackground(0, 0, 0)
+renWin.SetSize( 300 ,  300 )
+
+ #  When interaction starts, the requested frame rate is increased. 
+def  StartInteraction(obj, event):
+    global  renWin
+    renWin.SetDesiredUpdateRate( 10 )
+
+ #  When interaction ends, the requested frame rate is decreased to 
+#  normal levels. This causes a full resolution render to occur. 
+def  EndInteraction(obj, event):
+    global  renWin
+    renWin.SetDesiredUpdateRate( 0.001 )
+
+ #  The implicit function vtkPlanes is used in conjunction with the 
+#  volume ray cast mapper to limit which portion of the volume is 
+#  volume rendered. 
+planes  =  vtk.vtkPlanes()
+def  ClipVolumeRender(obj, event):
+    global  planes, volumeMapper
+    obj.GetPlanes(planes)
+    volumeMapper.SetClippingPlanes(planes)
+ 
+
+ #  Place the interactor initially. The output of the reader is used to 
+#  place the box widget. 
+boxWidget.SetInput(shifter.GetOutput())
+boxWidget.PlaceWidget()
+boxWidget.InsideOutOn()
+boxWidget.AddObserver( " StartInteractionEvent " , StartInteraction)
+boxWidget.AddObserver( " InteractionEvent " , ClipVolumeRender)
+boxWidget.AddObserver( " EndInteractionEvent " , EndInteraction)
+
+outlineProperty  =  boxWidget.GetOutlineProperty()
+outlineProperty.SetRepresentationToWireframe()
+outlineProperty.SetAmbient( 1.0 )
+outlineProperty.SetAmbientColor( 1 ,  1 ,  1 )
+outlineProperty.SetLineWidth( 3 )
+
+selectedOutlineProperty  =  boxWidget.GetSelectedOutlineProperty()
+selectedOutlineProperty.SetRepresentationToWireframe()
+selectedOutlineProperty.SetAmbient( 1.0 )
+selectedOutlineProperty.SetAmbientColor( 1 , 0, 0)
+selectedOutlineProperty.SetLineWidth( 3 )
+
+iren.Initialize()
+renWin.Render()
+iren.Start()
---
+++ index.py
@@ -0,0 +1,86 @@
+﻿import vtk
+reader = vtk.vtkDICOMImageReader()
+
+# 读入CT图像
+reader.SetDirectoryName('../CT2/test/')
+reader.Update()
+srange  =  reader.GetOutput().GetScalarRange()
+min  =  srange[0]
+max  =  srange[1]
+
+diff  =  max - min
+slope  =   40000 / diff
+inter  =   - slope * min
+shift  =  inter / slope
+
+shifter  =  vtk.vtkImageShiftScale()
+shifter.SetShift(shift)
+shifter.SetScale(slope)
+shifter.SetOutputScalarTypeToUnsignedShort()
+shifter.SetInputConnection(reader.GetOutputPort())
+shifter.ReleaseDataFlagOff()
+shifter.Update()
+shifterSrange = shifter.GetOutput().GetScalarRange()
+min2 = shifterSrange[0]
+max2 = shifterSrange[1]
+
+print  min,max,slope,inter,shift
+print min2,max2
+
+
+# print reader.GetOutput() # You should see that 3rd dimension is > 1 now
+# print reader.GetOutput().GetScalarRange()
+
+
+# Create the standard renderer, render window and interactor
+ren = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+# Create transfer mapping scalar value to opacity
+opacityTransferFunction = vtk.vtkPiecewiseFunction()
+opacityTransferFunction.AddPoint(0, 0)
+opacityTransferFunction.AddPoint(20000, 0)
+opacityTransferFunction.AddPoint(30000, 0.8)
+opacityTransferFunction.AddPoint(40000, 1)
+
+# Create transfer mapping scalar value to color
+colorTransferFunction = vtk.vtkColorTransferFunction()
+colorTransferFunction.AddRGBPoint(0, 0, 0, 0)
+colorTransferFunction.AddRGBPoint(40000, 1, 1, 1)
+
+# The property describes how the data will look
+volumeProperty = vtk.vtkVolumeProperty()
+volumeProperty.SetColor(colorTransferFunction)
+volumeProperty.SetScalarOpacity(opacityTransferFunction)
+volumeProperty.ShadeOn()
+volumeProperty.SetInterpolationTypeToLinear()
+
+# The mapper / ray cast function know how to render the data
+compositeFunction = vtk.vtkVolumeRayCastCompositeFunction()
+volumeMapper = vtk.vtkVolumeRayCastMapper()
+volumeMapper.SetVolumeRayCastFunction(compositeFunction)
+volumeMapper.SetInputConnection(shifter.GetOutputPort())
+
+# The volume holds the mapper and the property and
+# can be used to position/orient the volume
+volume = vtk.vtkVolume()
+volume.SetMapper(volumeMapper)
+volume.SetProperty(volumeProperty)
+
+ren.AddVolume(volume)
+ren.SetBackground(1, 1, 1)
+renWin.SetSize(600, 600)
+renWin.Render()
+
+def CheckAbort(obj, event):
+    if obj.GetEventPending() != 0:
+        obj.SetAbortRender(1)
+
+renWin.AddObserver("AbortCheckEvent", CheckAbort)
+
+iren.Initialize()
+renWin.Render()
+iren.Start()
